ac1f288528516bea7d9a10fbc9cc0e1db1bbabef
RafatAiub <tanvirmahtab1996@gmail.com>
2022-05-23 23:10:00 +0600

    create mongodb server and added

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+const uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-shard-00-00.ntump.mongodb.net:27017,cluster0-shard-00-01.ntump.mongodb.net:27017,cluster0-shard-00-02.ntump.mongodb.net:27017/?ssl=true&replicaSet=atlas-1jtay5-shard-0&authSource=admin&retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 app.get('/', (req, res) => {
     res.send('Hello From Doctor Uncle own portal!')
