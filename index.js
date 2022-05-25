98ebc89d29393c5824a4696e58d31fb64a2451ac
RafatAiub <tanvirmahtab1996@gmail.com>
2022-05-25 20:40:09 +0600

    user and admin route added

--- index.js
+++ index.js
@@ -32,12 +32,73 @@ async function run() {
     try {
         await client.connect();
         const toolCollection = client.db('tool_plaza').collection('tools');
+        const userCollection = client.db('tool_plaza').collection('users');
+
+        const verifyAdmin = async (req, res, next) => {
+            const requester = req.decoded.email;
+            const requesterAccount = await userCollection.findOne({ email: requester });
+            if (requesterAccount.role === 'admin') {
+                next();
+            }
+            else {
+                res.status(403).send({ message: 'forbidden' });
+            }
+        }
 
         app.get('/tools', async (req, res) => {
             const query = {};
             const cursor = toolCollection.find(query).project();
             const tools = await cursor.toArray();
             res.send(tools);
+        });
+
+        app.post('/tools', async (req, res) => {
+            const tool = req.body;
+            const result = await toolCollection.insertOne(tool);
+            res.send(result);
+        });
+
+        app.get('/tools/:_id', async (req, res) => {
+            const id = req.params._id;
+            const query = { _id: ObjectId(id) };
+            const cursor = await toolCollection.findOne(query);
+
+            res.send(cursor);
+        });
+
+        app.get('/user', async (req, res) => {
+            const users = await userCollection.find().toArray();
+            res.send(users);
+        });
+
+        app.get('/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = await userCollection.findOne({ email: email });
+            // const isAdmin = user.role === 'admin';
+            // res.send({ admin: isAdmin })
+        })
+
+        app.put('/user/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const filter = { email: email };
+            const updateDoc = {
+                $set: { role: 'admin' },
+            };
+            const result = await userCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        })
+
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ result, token });
         });
 
 
